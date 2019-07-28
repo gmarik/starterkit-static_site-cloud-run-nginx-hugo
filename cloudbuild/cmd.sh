2ce7e43ca8b65500cec888f992c1b0aabcadcfbf
gmarik <gmarik@gmail.com>
2019-07-28 03:00:05 -0700

    add hugo cloudbuild

--- cloudbuild/cmd.sh
+++ cloudbuild/cmd.sh
@@ -6,24 +6,15 @@ cache_warmup() {
   docker pull ${_SITE_IMG_LATEST} || exit 0
 }
 
-hugo_build_image() {
-  docker build -t ${_HUGO_IMG} \
-    -f hugo/hugo.Dockerfile hugo/ \
-    --build-arg HUGO_VERSION=${_HUGO_VERSION}
-
-  docker push ${_HUGO_IMG}
-}
-
 hugo_compile() {
-  docker run -w /site -v $PWD:/site ${_HUGO_IMG} -s ./src --minify
-  # hugo -s ./src --minify
+  curl -L https://github.com/gohugoio/hugo/releases/download/v0.56.0/hugo_extended_0.56.0_Linux-64bit.tar.gz|tar -xz
+  ./hugo -s ./$_SITE_NAME --minify
 }
 
 image_build() {
   docker build \
     --cache-from ${_SITE_IMG_LATEST} \
     --build-arg SITE=site01 \
-    --build-arg SRC=site01.content \
     -t ${_SITE_IMG} \
     -t ${_SITE_IMG_LATEST} \
     -f nginx/nginx.Dockerfile \
@@ -36,6 +27,8 @@ image_push() {
 
 site_image_build() {
   cache_warmup
+  # with hugo
+  test -f ./$_SITE_NAME/config.toml && hugo_compile
   image_build
   image_push
 }
